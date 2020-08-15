e65e794af888a3e3b647d4092c32909a9baa761a
Weslley Fernandes <54727047+WeslleyFrancis@users.noreply.github.com>
2020-08-15 18:10:13 -0300

    Fen Chess
    Construção de um Text Field usando ReGex, capaz de ler uma Fen Notation.

--- xadrezinho/lib/main.dart
+++ xadrezinho/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:xadrezinho/screens/Board.dart';
+import 'package:xadrezinho/screens/Leitor.dart';
 
 void main() => runApp(new MasterChessBoard());
 
 class MasterChessBoard extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,17 +17,46 @@ class MasterChessBoard extends StatelessWidget {
           title: Text('Master Chess',
               style: TextStyle(fontSize: 24.0, color: Colors.white)),
         ),
-        body: Board([
-          ['rook','knight','bishop','queen','king','bishop','knight','rook'],
-          ['pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn'],
-          [null,null,null,null,null,null,null,null],
-          [null,null,null,null,null,null,null,null],
-          [null,null,null,null,null,null,null,null],
-          [null,null,null,null,null,null,null,null],
-          ['pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn'],
-          ['rook','knight','bishop','queen','king','bishop','knight','rook'],
-          ]),
-      ),
-    );
+        body: ListView(
+          children: <Widget>[
+            Container(
+              height: 400,
+              child: Board([
+                ['rook','knight','bishop','queen','king','bishop','knight','rook'],
+                ['pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn'],
+                [null,null,null,null,null,null,null,null],
+                [null,null,null,null,null,null,null,null],
+                [null,null,null,null,null,null,null,null],
+                [null,null,null,null,null,null,null,null],
+                ['pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn'],
+                ['rook','knight','bishop','queen','king','bishop','knight','rook'],
+              ]),
+            ),
+            Container(
+              child: Leitor()
+              )
+          ]
+         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+
+          },
+        ),
+        ),
+      );
   }
 }
+
+
+//Board([
+//['rook','knight','bishop','queen','king','bishop','knight','rook'],
+//['pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn'],
+//[null,null,null,null,null,null,null,null],
+//[null,null,null,null,null,null,null,null],
+//[null,null,null,null,null,null,null,null],
+//[null,null,null,null,null,null,null,null],
+//['pawn','pawn','pawn','pawn','pawn','pawn','pawn','pawn'],
+//['rook','knight','bishop','queen','king','bishop','knight','rook'],
+//])
+
+//^(\w+)\/(\w+)\/(\w+)\/(\w+)\/(\w+)\/(\w+)\/(\w+)\/(\w+)\s(\w)+\s(\-|[KQkq]+)\s(\-|[\w]+)\s\d+\s\d+
